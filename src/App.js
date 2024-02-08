3c5f02525c67ff29d59733a2b616611cf5ba3818
mus_wolf <rustam161723@gmail.com>
2024-02-09 00:00:57 +0300

    third_day_commnets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 
 import './App.css';
 
-const welcome = {
-  greeting: 'Hola!',
-  title: 'React App 💟',
-};
-  
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walker',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ]
 
 function App() {
     return (
     <div className="App">
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
-      
+      <h1>My Hacker Stories</h1>
+<label htmlFor="search">Search: </label>
+<input id="search" type="text" />
+<hr />
+<ul>
+          {list.map(item => {
+            return (
+              <li key={item.objectID} style={{listStyle:'none'}}>
+                {item.title}
+          </li>
+        )
+      })}
+        </ul>
         
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+        <ul>
+{list.map(function (item) {
+return (
+<li key={item.objectID}>
+<span style={{marginRight:'10px'}}>
+<a href={item.url}>{item.title}</a>
+</span>
+<span style={{marginRight:'10px'}}>{item.author}</span>
+<span style={{marginRight:'10px'}}>{item.num_comments}</span>
+<span style={{marginRight:'10px'}}>{item.points}</span>
+</li>
+);
+})}
+</ul>
+          
      
     </div>
   );
 }
 
 export default App;
+
+/**
+ * React will display each item now, but you can still improve your code so React handles dynamic lists
+more gracefully. By assigning a --key-- attribute to each list item’s element, React can identify items if
+the list changes (e.g. re-ordering). The --key-- isn’t necessary yet in our current situation, however, it’s
+a best practice to use it from the start. Fortunately, our items come with an identifier:
+ */
+
+/**
+ * 
+ */
