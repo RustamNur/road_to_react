b9a6698a6620f3048461a7a2a1977565ed8236c3
mus_wolf <rustam161723@gmail.com>
2024-02-27 18:13:54 +0300

    add some comments...

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,3 +14,16 @@ const Modal = ({children,data}) => {
 };
 
 export default Modal;
+
+/**
+ * New Portal ==> publicdagi html file'da bizda bittagina
+ * id='root' joylashgan bo'lib Reactdagi hamma narsa manashu id ichiga kelib
+ * tushadi . biz yangi portal ochish bilan anashu #root'dan tashqarida
+ * componentimizni ajratilgan holatda devOpsda element'da ko'ra olamiz.
+ * 
+ * Buning uchun biz react-dom'dan ReactDOM ni import qilib olamiz va 
+ * component returnida ReactDOM.createPortal ((),document.body) korinishida kod
+ * yozamiz.
+ * 
+ * () -> return bo'ladigan kodlarimiz shu qavslar ichida joylashgan boladi.
+ */
